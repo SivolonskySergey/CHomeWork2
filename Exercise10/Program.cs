84da686e76a6888d023d77a18288ef9093b447e4
Sergey <bibert.shmibert@gmail.com>
2022-09-20 14:51:02 +0400

    add exercise 10

---
+++ Exercise10/Program.cs
@@ -0,0 +1,11 @@
+﻿
+    void SecondDigit() 
+    {
+        Random random = new Random();
+        int number = random.Next(100, 1000);
+        Console.WriteLine(number);
+        Console.WriteLine((number / 10) % 10);
+    }
+    SecondDigit();
+
+
