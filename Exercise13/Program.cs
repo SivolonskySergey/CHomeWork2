21d6a9e38ea9a4100afaa4c19530420ab9434ff6
Sergey <bibert.shmibert@gmail.com>
2022-09-21 15:26:21 +0400

    add exercise 13

---
+++ Exercise13/Program.cs
@@ -0,0 +1,18 @@
+﻿void thirdDigit() {
+    Console.WriteLine("Введите любое число: ");
+    string num = Console.ReadLine();
+    int lengthOfNum = num.Length;
+    int number = Convert.ToInt32(num);
+    if (lengthOfNum < 3) {
+        Console.WriteLine("Третьей цифры нет!");
+    }
+    else {
+            lengthOfNum = lengthOfNum - 3;
+            int divider = Convert.ToInt32(Math.Pow(10, lengthOfNum));
+            number = (number / divider) % 10;
+            Console.WriteLine(number);
+    }
+}
+
+
+thirdDigit();
