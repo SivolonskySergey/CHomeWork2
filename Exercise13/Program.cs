9fcbe1934cdf74d8dfb4ff20350e265fd73e5be5
Sergey <bibert.shmibert@gmail.com>
2022-09-21 16:23:04 +0400

    exercise 15 changes

--- Exercise13/Program.cs
+++ Exercise13/Program.cs
@@ -1,16 +1,16 @@
 ﻿void thirdDigit() {
     Console.WriteLine("Введите любое число: ");
     string num = Console.ReadLine();
-    int lengthOfNum = num.Length;
-    int number = Convert.ToInt32(num);
+    int number = Math.Abs(Convert.ToInt32(num));
+    int lengthOfNum = Convert.ToString(number).Length;
     if (lengthOfNum < 3) {
         Console.WriteLine("Третьей цифры нет!");
     }
     else {
-            lengthOfNum = lengthOfNum - 3;
-            int divider = Convert.ToInt32(Math.Pow(10, lengthOfNum));
-            number = (number / divider) % 10;
-            Console.WriteLine(number);
+        lengthOfNum = lengthOfNum - 3;
+        int divider = Convert.ToInt32(Math.Pow(10, lengthOfNum));
+        number = (number / divider) % 10;
+        Console.WriteLine(number);
     }
 }
 
